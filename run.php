a216e16aea2735b8b8a45def903df320b1492c75
Wahyu Arif Purnomo <warifp@gmail.com>
2020-01-08 10:41:27 +0700

    Update run.php

--- run.php
+++ run.php
@@ -3,8 +3,8 @@ error_reporting(0);
 /**
  * Author  : Wahyu Arif Purnomo
  * Name    : Proxy Checker
- * Version : 1.0
- * Update  : 14 June 2019
+ * Version : 2.0
+ * Update  : 08 Januari 2020
  * 
  * If you are a reliable programmer or the best developer, please don't change anything.
  * If you want to be appreciated by others, then don't change anything in this script.
@@ -32,49 +32,34 @@ for ($a = 0; $a < count($data); $a++) {
     $no++;
 
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, 'https://checkerproxy.net/api/check');
+    curl_setopt($ch, CURLOPT_URL, 'http://checkip.dyndns.org/');
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, "{\"type\":2,\"timeout\":20,\"publish\":false,\"proxies\":[\"$proxy\"]}");
     curl_setopt($ch, CURLOPT_POST, 1);
-    
-    $headers   = array();
-    $headers[] = 'Host: checkerproxy.net';
-    $headers[] = 'Connection: keep-alive';
-    $headers[] = 'Content-Length: 73';
-    $headers[] = 'Origin: https://checkerproxy.net';
-    $headers[] = 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36';
-    $headers[] = 'UContent-Type: text/plain;charset=UTF-8';
-    $headers[] = 'Accept: */*';
-    $headers[] = 'Referer: https://checkerproxy.net/';
-    $headers[] = 'Accept-Encoding: gzip, deflate, br';
-    $headers[] = 'Accept-Language: id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7';
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_PROXY, $proxy);
     
     $result = curl_exec($ch);
     curl_close($ch);
-    $decode = json_decode($result);
-    $error = $decode->error;
-    if($error == "couldn't read payload"){
-        $climate->error( $no . '. Die  | ' . $proxy . ' !Response :' . $error . '');
+    if($result == "error code: 1001"){
+        $climate->error( $no . '. Die  | ' . $proxy);
         $save = fopen($save_die, 'a');
         fwrite($save, $proxy . "\n");
     } else if($result == null){
-        $climate->error( $no . '. Die  | ' . $proxy . ' !Response : no response from your proxy list');
+        $climate->error( $no . '. Die  | ' . $proxy);
         $save = fopen($save_die, 'a');
         fwrite($save, $proxy . "\n");
     } else {
-        $climate->info( $no . '. Live | ' . $proxy .  ' !Response : ' . $result . '');
+        $climate->info( $no . '. Live | ' . $proxy);
         $save = fopen($save_live, 'a');
         fwrite($save, $proxy . "\n");
     }
 }
-$climate->br()->shout('Done, your result live saved in folder : "' . $save_live . '" and result die saved in folder "' . $save_die .'".');
+$climate->br()->info('Done, your result live saved in folder : "' . $save_live . '" and result die saved in folder "' . $save_die .'".');
 
 /**
  * Author  : Wahyu Arif Purnomo
  * Name    : Proxy Checker
- * Version : 1.0
- * Update  : 14 June 2019
+ * Version : 2.0
+ * Update  : 08 Januari 2020
  * 
  * If you are a reliable programmer or the best developer, please don't change anything.
  * If you want to be appreciated by others, then don't change anything in this script.
